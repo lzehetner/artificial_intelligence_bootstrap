dbc81da52a3beae63911927ac23c45df8da38a08
Leon Zehetner <95644720+lzehetner@users.noreply.github.com>
2023-06-01 13:19:33 +0200

    trying out API

--- script.js
+++ script.js
@@ -1,61 +1,31 @@
-const facts = {
-"Tiere": [
-    "Es gibt mehr als 1 Million bekannter Insektenarten auf der Welt.",
-    "Krokodile haben einen starken Biss, der bis zu 3700 psi erreichen kann.",
-    "Pandas sind keine Bären, sondern gehören zur Familie der Bärenartigen.",
-],
-"Wissenschaft": [
-    "Der längste Tag des Jahres, der Sommer-Sonnenwende, ist auch der Tag, an dem die Erdachse am stärksten geneigt ist.",
-    "Die Milchstraße hat einen Durchmesser von etwa 100.000 Lichtjahren.",
-    "Das menschliche Gehirn besteht aus etwa 100 Milliarden Neuronen.",
-],
-"Geschichte": [
-    "Die Antike wurde von den Griechen und Römern geprägt und dauerte von etwa 700 v. Chr. bis 500 n. Chr.",
-    "Die französische Revolution fand von 1789 bis 1799 statt und führte zur Abschaffung der Monarchie in Frankreich.",
-    "Im Zweiten Weltkrieg kämpften die Alliierten gegen die Achsenmächte.",
-],
-};
-
-// Erstellen Sie eine Liste von Kategorien.
-const categories = Object.keys(facts);
-
-// Funktion zum Generieren eines zufälligen Fakts.
-function generateFact() {
-const category = document.getElementById("category").value;
-const factList = facts[category];
-const randomFact = factList[Math.floor(Math.random() * factList.length)];
-document.getElementById("fact").innerHTML = randomFact;
-
-}
-
-// Funktion zum Ändern der Kategorie.
-function addFact() {
-const categoryElement = document.getElementById("category");
-const currentCategory = categoryElement.value;
-let newCategory;
-do {
-    newCategory = categories[Math.floor(Math.random() * categories.length)];
-} while (newCategory === currentCategory);
-categoryElement.value = newCategory;
-generateFact();
-}
-
-// Initialisieren Sie die Kategorieauswahl.
-const categorySelect = document.createElement("select");
-categorySelect.id = "category";
-for (const category of categories) {
-const option = document.createElement("option");
-option.value = category;
-option.text = category;
-categorySelect.appendChild(option);
-}
-document.body.insertBefore(categorySelect, document.getElementById("fact"));
-
-function toggleText() {
-    var text = document.getElementById("addFactText");
-    if (text.style.display === "none") {
-      text.style.display = "none";
-    } else {
-      text.style.display = "none";
-    }
+document.addEventListener('DOMContentLoaded', function() {
+  const generateButton = document.getElementById('generateButton');
+  const factDisplay = document.getElementById('fact');
+  
+  generateButton.addEventListener('click', function() {
+    fetchNASAFact()
+      .then(function(fact) {
+        factDisplay.textContent = fact;
+      })
+      .catch(function(error) {
+        console.error(error);
+        factDisplay.textContent = 'Fehler beim Laden des Fakts.';
+      });
+  });
+  
+  function fetchNASAFact() {
+    const apiUrl = 'https://api.nasa.gov/planetary/apod?api_key=sFBaYVdNpxvCpXPACNwxMPGP6MoIVCnSSaqP3QIA';
+    
+    return fetch(apiUrl)
+      .then(function(response) {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('Fehler bei der API-Anfrage: ' + response.status);
+        }
+      })
+      .then(function(data) {
+        return data.explanation;
+      });
   }
+});
